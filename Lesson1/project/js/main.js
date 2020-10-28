bb73729407a239ad7cfc4ea98be301dedb48f1aa
Bopobeii <69038342+Bopobeii@users.noreply.github.com>
2020-10-28 19:36:24 +0300

    дз по 1 уроку

--- Lesson1/project/js/main.js
+++ Lesson1/project/js/main.js
@@ -5,19 +5,22 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price, img = '') => {
-  return `<div class="product-item">
-            <img src="${img}" alt="">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button class="by-btn">Добавить в корзину</button>
+const renderProduct = (id = 0, title = '', price = 0, img='https://placehold.it/150x100') => {
+  return `<div class="product-item" data-id="${id}">
+<img class="product-item__img" src="${img}" alt="">
+            <h3 class="product-item__title">${title}</h3>
+            <p class="product-item__price">${price}</p>
+            <button class="product-item__by-btn">Добавить в корзину</button>
           </div>`;
 };
 
-const renderProducts = (list) => {
-  const productList = list.map(item => renderProduct(item.title, item.price));
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+const renderProducts = (list = []) => {
+
+  list.forEach(item => {
+  let prod = renderProduct(item.id, item.title, item.price)
+  document.querySelector('.products').insertAdjacentHTML('beforeend', prod);
+  console.log(prod);
+}); 
 };
 
 renderProducts(products);
